f056375367a31c99f04af852353453cc5c13634a
Nikita Solovyov <nikita.solovyov@unacast.com>
2025-02-27 15:09:11 +0100

    Adding cofig and Dockerfile changes

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
+$db_host = '35.198.119.60';
 $db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_user = 'appmod-phpapp-user';
+$db_pass = '123321';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
